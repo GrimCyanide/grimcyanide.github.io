de0b8aaf44d77902b382a216f82f61c44dd6381f
Grim_Cyanide <joshuacouchman@hotmail.co.uk>
2026-06-03 11:14:25 +0100

    Update steam-games.js

--- api/steam-games.js
+++ api/steam-games.js
@@ -1,18 +1,25 @@
 export default async function handler(req, res) {
-  const { steamid } = req.query;
+  const { steamid, type } = req.query;
   const STEAM_API_KEY = process.env.STEAM_API_KEY;
 
   if (!steamid) {
     return res.status(400).json({ error: 'Steam ID required' });
   }
 
-  try {
-    const response = await fetch(`https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1/?key=${STEAM_API_KEY}&steamid=${steamid}&count=5`);
-    const data = await response.json();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  try {
+    let url;
+    if (type === 'owned') {
+      url = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${STEAM_API_KEY}&steamid=${steamid}&include_appinfo=true&include_played_free_games=true`;
+    } else {
+      url = `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1/?key=${STEAM_API_KEY}&steamid=${steamid}&count=5`;
+    }
+
+    const response = await fetch(url);
+    const data = await response.json();
 
     if (data.response && data.response.games) {
       res.status(200).json({ games: data.response.games });
