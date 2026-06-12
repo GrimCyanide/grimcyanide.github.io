03d862a0f11870a5b600a3977295ac60c6dd09c3
Grim_Cyanide <joshuacouchman@hotmail.co.uk>
2026-06-12 13:58:38 +0100

    Update twitch.js

--- api/twitch.js
+++ api/twitch.js
@@ -2,7 +2,6 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   const { endpoint, channel } = req.query;
-
   try {
     if (endpoint === 'streams') {
       const response = await fetch(`https://www.twitch.tv/${channel}`);
